b69836108b054f72621cdc186b642a8a976910f6
Lareen Melo <lareen.melo@disney.com>
2021-05-13 07:27:18 -0400

    Add Day 32/100

---
+++ PROJECT6/Animations/Animations/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Melo, Lareen on 5/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(Color.purple)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
