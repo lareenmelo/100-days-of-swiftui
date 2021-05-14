1198a75f6bc2cf66077276faa3386d8dc26b9bed
Lareen Melo <lareen.melo@disney.com>
2021-05-14 05:56:01 -0400

    Add Day 33/100

--- PROJECT6/Animations/Animations/ContentView.swift
+++ PROJECT6/Animations/Animations/ContentView.swift
@@ -7,20 +7,38 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var animationAmount = 0.0
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
     
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
+struct ContentView: View {
+    @State private var isShowingPurple = false
     var body: some View {
-        Button("Tap Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                self.animationAmount += 360
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    self.isShowingPurple.toggle()
+                }
+            }
+            
+            if isShowingPurple {
+                Rectangle()
+                    .fill(Color.purple)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
         }
-        .padding(50)
-        .background(Color.purple)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
