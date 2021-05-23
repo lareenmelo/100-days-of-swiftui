82a270ff14a7a2d975afebed7329b586fac0053c
Lareen Melo <lareen.melo@disney.com>
2021-05-22 22:36:24 -0400

    Add Day 39/100

---
+++ PROJECT8/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Melo, Lareen on 5/22/21.
+//
+
+import SwiftUI
+
+struct User: Codable {
+    var name: String
+    var address: Address
+}
+
+struct Address: Codable {
+    var street: String
+    var city: String
+}
+
+struct ContentView: View {
+    var body: some View {
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PROJECT8/README.md
@@ -0,0 +1,22 @@
+# Project #8 - iExpense
+
+https://www.hackingwithswift.com/100/swiftui/39
+
+> ... an app that lets users learn about the missions and astronauts that formed NASA’s Apollo space program
+
+## Topics
+NavigationLink, Codable, ScrollView, GeometricReader
+
+|Day 39 :white_check_mark: | Day 40 :white_check_mark: | Day 41 :white_check_mark: |
+|:--|:--|:--|
+| learned about geometricreader, navigationlinks and scrollviews  |  |  |
+|![D39](Data/D39.mov)|![D40](Data/D40.mov)|![D41](Data/D41.mov)|
+
+## Challenges
+
+From [Hacking with Swift]():
+>1.
+>2.
+>3.
+
+
