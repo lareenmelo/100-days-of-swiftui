028417fb2dc3cb731cb98422114d3756b83a0e66
Lareen Melo <lareen.melo@disney.com>
2021-05-24 21:04:24 -0400

    Add Day 43/100

---
+++ PROJECT9/Drawing/Drawing/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Melo, Lareen on 5/24/21.
+//
+
+import SwiftUI
+
+struct Arc: InsettableShape {
+    var insetAmount: CGFloat = 0
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(Color.blue, lineWidth: 40)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PROJECT9/README.md
@@ -0,0 +1,18 @@
+# Project #9 - Drawing
+
+https://www.hackingwithswift.com/100/swiftui/43
+
+## Topics
+Shape(), InsettableShape, Path, strokeBorder()
+
+|Day 43 :white_check_mark: | Day 44 :white_check_mark: | Day 45 :white_check_mark: | Day 46 :white_check_mark: |
+|:--|:--|:--|:--|
+| learned how to draw shapes using Path, worked with StrokeStyle(), and InsettableShape()  |  |  |
+|![D43](Data/D43.png)|![D44](Data/D44.mov)|![D45](Data/D45.mov)|![D46](Data/D46.mov)
+
+## Challenges
+
+From [Hacking with Swift]():
+>1. 
+>2.
+>3. 
