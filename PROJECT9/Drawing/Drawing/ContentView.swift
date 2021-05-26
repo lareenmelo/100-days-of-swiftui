8912f512eee11bef2df4054fa300a9b87018cd55
Lareen Melo <lareen.melo@disney.com>
2021-05-25 20:06:11 -0400

    Add Day 44/100

--- PROJECT9/Drawing/Drawing/ContentView.swift
+++ PROJECT9/Drawing/Drawing/ContentView.swift
@@ -7,34 +7,42 @@
 
 import SwiftUI
 
-struct Arc: InsettableShape {
-    var insetAmount: CGFloat = 0
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
+struct Flower: Shape {
+    var petalOffset: Double = -20
+    var petalWidth: Double = 100
 
     func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-
         var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+            let rotatedPetal = originalPetal.applying(position)
+
+            path.addPath(rotatedPetal)
+        }
 
         return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
     }
 }
 
 struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(Color.blue, lineWidth: 40)
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .topLeading, endPoint: .trailing), style: FillStyle(eoFill: true))
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding([.horizontal])
+            
+        }
         
     }
 }
