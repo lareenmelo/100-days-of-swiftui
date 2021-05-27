bd4a5ad63ebb7439b7b20a4c96bf75d2750435e6
Lareen Melo <lareen.melo@disney.com>
2021-05-27 00:15:48 -0400

    Add Day 45/100

--- PROJECT9/Drawing/Drawing/ContentView.swift
+++ PROJECT9/Drawing/Drawing/ContentView.swift
@@ -7,43 +7,95 @@
 
 import SwiftUI
 
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: CGFloat
 
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: CGFloat(value))
-                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
-                        self.color(for: value, brightness: 1),
-                        self.color(for: value, brightness: 0.5)
-                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)            }
+    // gcd for the inner and outer radius
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+        
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
         }
-        .drawingGroup()
+        
+        return a
     }
+    
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
+        let outerRadius = CGFloat(self.outerRadius)
+        let innerRadius = CGFloat(self.innerRadius)
+        let distance = CGFloat(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * CGFloat.pi * outerRadius / CGFloat(divisor)) * amount
 
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(self.steps) + self.amount
+        var path = Path()
 
-        if targetHue > 1 {
-            targetHue -= 1
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+
+            x += rect.width / 2
+            y += rect.height / 2
+
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
         }
 
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+        return path
+        
     }
 }
 
 struct ContentView: View {
-    @State private var colorCycle = 0.0
-
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount: CGFloat = 1.0
+    @State private var hue = 0.6
+    
     var body: some View {
-        VStack {
-            ColorCyclingCircle(amount: self.colorCycle)
+        VStack(spacing: 0) {
+            Spacer()
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
                 .frame(width: 300, height: 300)
 
-            Slider(value: $colorCycle)
+            Spacer()
+            
+            Group {
+                Text("Inner radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Outer radius: \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Distance: \(Int(distance))")
+                Slider(value: $distance, in: 1...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Amount: \(amount, specifier: "%.2f")")
+                Slider(value: $amount)
+                    .padding([.horizontal, .bottom])
+
+                Text("Color")
+                Slider(value: $hue)
+                    .padding(.horizontal)
+
+
+            }
+            
         }
     }
 }
