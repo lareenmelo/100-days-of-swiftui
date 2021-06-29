389b3caaf0fed4a48fc92f7ec455fb7b9d12e061
lareenmelo <lareenmmelo@gmail.com>
2021-06-28 21:39:21 -0400

    Add Day 62/100

---
+++ PROJECT13/Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Lareen Melo on 6/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
+    
+    var body: some View {
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                self.showingActionSheet = true
+            }
+            .actionSheet(isPresented: $showingActionSheet) {
+                ActionSheet(title: Text("Change background"), message: Text("Select a new color"), buttons: [
+                    .default(Text("Red")) { self.backgroundColor = .red },
+                    .default(Text("Green")) { self.backgroundColor = .green },
+                    .default(Text("Blue")) { self.backgroundColor = .blue },
+                    .cancel()
+                ])
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
