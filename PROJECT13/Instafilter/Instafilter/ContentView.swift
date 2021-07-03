2305fff704093d1d0e9d1f6839f450b35384b480
lareenmelo <lareenmmelo@gmail.com>
2021-07-02 22:13:24 -0400

    Add Day 66/100

--- PROJECT13/Instafilter/Instafilter/ContentView.swift
+++ PROJECT13/Instafilter/Instafilter/ContentView.swift
@@ -14,8 +14,11 @@ struct ContentView: View {
     @State private var filterIntensity = 0.5
     @State private var inputImage: UIImage?
     @State private var showingImagePicker = false
-    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     let context = CIContext()
+    @State private var showingFilterSheet = false
+    @State private var processedImage: UIImage?
+
 
     var body: some View {
         let intensity = Binding<Double>(
@@ -56,13 +59,16 @@ struct ContentView: View {
                 
                 HStack {
                     Button("Change Filter") {
-                        // change filter
+                        self.showingFilterSheet = true
                     }
                     
                     Spacer()
                     
                     Button("Save") {
-                        
+                        guard let processedImage = self.processedImage else { return }
+
+                        let imageSaver = ImageSaver()
+                        imageSaver.writeToPhotoAlbum(image: processedImage)
                     }
                 }
             }
@@ -72,6 +78,23 @@ struct ContentView: View {
         .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
             ImagePicker(image: self.$inputImage)
         }
+        .actionSheet(isPresented: $showingFilterSheet) {
+            ActionSheet(title: Text("Select a filter"), buttons: [
+                .default(Text("Crystallize")) { self.setFilter(CIFilter.crystallize()) },
+                .default(Text("Edges")) { self.setFilter(CIFilter.edges()) },
+                .default(Text("Gaussian Blur")) { self.setFilter(CIFilter.gaussianBlur()) },
+                .default(Text("Pixellate")) { self.setFilter(CIFilter.pixellate()) },
+                .default(Text("Sepia Tone")) { self.setFilter(CIFilter.sepiaTone()) },
+                .default(Text("Unsharp Mask")) { self.setFilter(CIFilter.unsharpMask()) },
+                .default(Text("Vignette")) { self.setFilter(CIFilter.vignette()) },
+                .cancel()
+            ])
+        }
+    }
+    
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
     }
     
     func loadImage() {
@@ -83,13 +106,17 @@ struct ContentView: View {
     }
     
     func applyProcessing() {
-        currentFilter.intensity = Float(filterIntensity)
-
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        
         guard let outputImage = currentFilter.outputImage else { return }
 
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
+            processedImage = uiImage
         }
     }
 }
---
+++ PROJECT13/Instafilter/Instafilter/ImageSaver.swift
@@ -0,0 +1,25 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Lareen Melo on 7/2/21.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
