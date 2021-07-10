6eba7742878e3c8d95fd50bb08dea15904b3afe1
lareenmelo <lareenmmelo@gmail.com>
2021-07-10 01:54:57 -0400

    Add Day 73/100

--- PROJECT14/BucketList/BucketList/ContentView.swift
+++ PROJECT14/BucketList/BucketList/ContentView.swift
@@ -9,55 +9,50 @@ import SwiftUI
 import MapKit
 import LocalAuthentication
 
-struct ContentView: View {
+// 2
+struct UnlockedView: View {
     @State private var centerCoordinate = CLLocationCoordinate2D()
     @State private var locations = [CodableMKPointAnnotation]()
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
     @State private var showingEditScreen = false
-    @State private var isUnlocked = false
 
-    
     var body: some View {
-        ZStack {
-            if isUnlocked {
-                MapView(centerCoordinate: $centerCoordinate, annotations: locations, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
-                    .edgesIgnoringSafeArea(.all)
-                Circle()
-                    .fill(Color.blue)
-                    .opacity(0.3)
-                    .frame(width: 32, height: 32)
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            let newLocation = CodableMKPointAnnotation()
-                            newLocation.coordinate = self.centerCoordinate
-                            newLocation.title = "Example location"
-                            
-                            self.locations.append(newLocation)
-                            self.selectedPlace = newLocation
-                            self.showingEditScreen = true
-                        }) {
-                            Image(systemName: "plus")
-                        }
+        MapView(centerCoordinate: $centerCoordinate, annotations: locations, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
+            .edgesIgnoringSafeArea(.all)
+        Circle()
+            .fill(Color.blue)
+            .opacity(0.3)
+            .frame(width: 32, height: 32)
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action: {
+                    let newLocation = CodableMKPointAnnotation()
+                    newLocation.coordinate = self.centerCoordinate
+                    newLocation.title = "Example location"
+                    
+                    self.locations.append(newLocation)
+                    self.selectedPlace = newLocation
+                    self.showingEditScreen = true
+                }) {
+                    // 1
+                    Image(systemName: "plus")
                         .padding()
                         .background(Color.black.opacity(0.75))
                         .foregroundColor(.white)
                         .font(.title)
                         .clipShape(Circle())
                         .padding(.trailing)
-                    }
+                    
+                    /*
+                     This is working as expected because before we were
+                     setting the background surrounding the button (Frame(Buton)). Instead,
+                     right now, we're setting the shaping and styling within
+                     the button so the hole image/visual button display behaves as a butotn (Button(Frame))
+                     */
                 }
-            } else {
-                Button("Unlock Places") {
-                    self.authenticate()
-                }
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .clipShape(Capsule())
             }
         }
         .onAppear(perform: loadData)
@@ -98,6 +93,33 @@ struct ContentView: View {
             print("Unable to save data.")
         }
     }
+}
+
+struct ContentView: View {
+    @State private var isUnlocked = false
+    // 3
+    @State private var showingAuthenticationAlert = false
+    @State private var authenticationError = ""
+    
+    var body: some View {
+        ZStack {
+            if isUnlocked {
+                UnlockedView()
+            } else {
+                Button("Unlock Places") {
+                    self.authenticate()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+            }
+        }
+        // 3
+        .alert(isPresented: $showingAuthenticationAlert) {
+            Alert(title: Text("Authentication error"), message: Text(self.authenticationError), dismissButton: .default(Text("OK")))
+        }
+    }
     
     func authenticate() {
         let context = LAContext()
@@ -112,12 +134,16 @@ struct ContentView: View {
                     if success {
                         self.isUnlocked = true
                     } else {
-                        // error
+                        // 3
+                        self.authenticationError = "\(authenticationError?.localizedDescription ?? "Unknown error.")"
+                        self.showingAuthenticationAlert = true
                     }
                 }
             }
         } else {
-            // no biometrics
+            // 3
+            self.authenticationError = "Oh no! No biometrics available uvu"
+            self.showingAuthenticationAlert = true
         }
     }
 }
--- PROJECT14/README.md
+++ PROJECT14/README.md
@@ -10,11 +10,11 @@ Comparable, operator overloading, FileManager, MVC, MapKit, LocalAuthentication,
 |Day 68 :white_check_mark: | Day 69 :white_check_mark: | Day 70 :white_check_mark: | Day 71 :white_check_mark: | Day 72 :white_check_mark: | Day 73 :white_check_mark: |
 |:--|:--|:--|:--|:--|:--|
 | learned about Comparable, operator overloading, FileManager, writing data to the documents directory, and handling a View's state using Enums.  | worked with MapKit, UIViewRepresentable, and LocalAuthentication (used it to work with Touch ID and Face ID).  | worked more with MKMapView, MKPointAnnotation, got more comfortable with coordinators, and many MapKit delegate methods 😭. | continued with the project, we can now edit a location, and we added a section that displays nearby places to visit. This was used using URLSession to get data from Wikipedia. | worked with authentication today, now you have to use biometrics (if there's any) to access your saved locations and there's persistence (saving to the documents directory) |  |
-|  🗺  | ![D69](Data/D69.png)|  ![D70](https://user-images.githubusercontent.com/12801333/124700638-0ccdda00-debb-11eb-9092-560b34e0d472.mp4) | ![D71](https://user-images.githubusercontent.com/12801333/124851051-c6868280-df6f-11eb-8591-f29dae21fc92.mp4)| ![D72](https://user-images.githubusercontent.com/12801333/125012853-df0e9f80-e038-11eb-85a5-63f099639ad0.mp4) |  |  
+|  🗺  | ![D69](Data/D69.png)|  ![D70](https://user-images.githubusercontent.com/12801333/124700638-0ccdda00-debb-11eb-9092-560b34e0d472.mp4) | ![D71](https://user-images.githubusercontent.com/12801333/124851051-c6868280-df6f-11eb-8591-f29dae21fc92.mp4)| ![D72](https://user-images.githubusercontent.com/12801333/125012853-df0e9f80-e038-11eb-85a5-63f099639ad0.mp4) |  ![D73](Data/D73.mp4) |  
 
 ## Challenges
 
-From [Hacking with Swift]():
->1. 
->2. 
->3. 
+From [Hacking with Swift](https://www.hackingwithswift.com/books/ios-swiftui/bucket-list-wrap-up):
+>1. Our + button is rather hard to tap. Try moving all its modifiers to the image inside the button – what difference does it make, and can you think why?
+>2. Having a complex `if` condition in the middle of `ContentView` isn’t easy to read – can you rewrite it so that the `MapView`, `Circle`, and `Button` are part of their own view? This might take more work than you think!
+>3. Our app silently fails when errors occur during biometric authentication. Add code to show those errors in an alert, but be careful: you can only add one `alert()` modifier to each view.
