890904b8b3ad91068aabb161f25a9ddbaed3c8af
lareenmelo <lareenmmelo@gmail.com>
2021-07-18 20:36:13 -0400

    Add Day 82/100

---
+++ PROJECT16/HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Lareen Melo on 7/18/21.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    let id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+
+    init() {
+        self.people = []
+    }
+}
---
+++ PROJECT16/HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,71 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Lareen Melo on 7/18/21.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    let filter: FilterType
+    @EnvironmentObject var prospects: Prospects
+
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+    
+    var filteredProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationBarTitle(title)
+            .navigationBarItems(trailing: Button(action: {
+                    let prospect = Prospect()
+                    prospect.name = "Paul Hudson"
+                    prospect.emailAddress = "paul@hackingwithswift.com"
+                    self.prospects.people.append(prospect)
+                }) {
+                    Image(systemName: "qrcode.viewfinder")
+                    Text("Scan")
+                })
+        }
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView(filter: .none)
+    }
+}
