2ec26cebd31cd4f66f8a32bd77917af644de9a41
lareenmelo <lareenmmelo@gmail.com>
2021-07-26 18:13:44 -0400

    Add Day 90 / 100

--- PROJECT17/Flashzilla/Flashzilla/CardView.swift
+++ PROJECT17/Flashzilla/Flashzilla/CardView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityEnabled) var accessibilityEnabled
+
     let card: Card
+
     var removal: (() -> Void)? = nil
+
+    @State private var feedback = UINotificationFeedbackGenerator()
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
-    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-
 
     var body: some View {
         ZStack {
@@ -34,14 +38,20 @@ struct CardView: View {
                 .shadow(radius: 10)
 
             VStack {
-                Text(card.prompt)
-                    .font(.largeTitle)
-                    .foregroundColor(.black)
-                
-                if isShowingAnswer {
-                    Text(card.answer)
-                        .font(.title)
-                        .foregroundColor(.gray)
+                if accessibilityEnabled {
+                    Text(isShowingAnswer ? card.answer : card.prompt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                } else {
+                    Text(card.prompt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundColor(.gray)
+                    }
                 }
             }
             .padding(20)
@@ -51,14 +61,22 @@ struct CardView: View {
         .rotationEffect(.degrees(Double(offset.width / 5)))
         .offset(x: offset.width * 5, y: 0)
         .opacity(2 - Double(abs(offset.width / 50)))
+        .accessibility(addTraits: .isButton)
         .gesture(
             DragGesture()
-                .onChanged { gesture in
-                    self.offset = gesture.translation
+                .onChanged { offset in
+                    self.offset = offset.translation
+                    self.feedback.prepare()
                 }
 
                 .onEnded { _ in
                     if abs(self.offset.width) > 100 {
+                        if self.offset.width > 0 {
+                            self.feedback.notificationOccurred(.success)
+                        } else {
+                            self.feedback.notificationOccurred(.error)
+                        }
+
                         self.removal?()
                     } else {
                         self.offset = .zero
@@ -68,6 +86,7 @@ struct CardView: View {
         .onTapGesture {
             self.isShowingAnswer.toggle()
         }
+        .animation(.spring())
     }
 }
 
--- PROJECT17/Flashzilla/Flashzilla/ContentView.swift
+++ PROJECT17/Flashzilla/Flashzilla/ContentView.swift
@@ -8,20 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var cards = [Card](repeating: Card.example, count: 10)
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-    @State private var timeRemaining = 100
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    @State private var isActive = true
+    @Environment(\.accessibilityEnabled) var accessibilityEnabled
 
+    @State private var cards = [Card]()
+    @State private var showingEditScreen = false
+
+    @State private var timeRemaining = 100
+    @State private var isActive = true
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
         ZStack {
-            Image("background")
+            Image(decorative: "background")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
-            
+
             VStack {
                 Text("Time: \(timeRemaining)")
                     .font(.largeTitle)
@@ -33,7 +36,7 @@ struct ContentView: View {
                             .fill(Color.black)
                             .opacity(0.75)
                     )
-                
+
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: self.cards[index]) {
@@ -42,6 +45,8 @@ struct ContentView: View {
                            }
                         }
                         .stacked(at: index, in: self.cards.count)
+                        .allowsHitTesting(index == self.cards.count - 1)
+                        .accessibility(hidden: index < self.cards.count - 1)
                     }
                 }
                 .allowsHitTesting(timeRemaining > 0)
@@ -54,32 +59,63 @@ struct ContentView: View {
                         .clipShape(Capsule())
                 }
             }
-            if differentiateWithoutColor {
-                VStack {
+
+            VStack {
+                HStack {
                     Spacer()
 
-                    HStack {
-                        Image(systemName: "xmark.circle")
-                            .padding()
-                            .background(Color.black.opacity(0.7))
-                            .clipShape(Circle())
-                        Spacer()
-                        Image(systemName: "checkmark.circle")
+                    Button(action: {
+                        self.showingEditScreen = true
+                    }) {
+                        Image(systemName: "plus.circle")
                             .padding()
                             .background(Color.black.opacity(0.7))
                             .clipShape(Circle())
                     }
+                }
+
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .padding()
+
+            if differentiateWithoutColor || accessibilityEnabled {
+                VStack {
+                    Spacer()
+
+                    HStack {
+                        Button(action: {
+                            withAnimation {
+                                self.removeCard(at: self.cards.count - 1)
+                            }
+                        }) {
+                            Image(systemName: "xmark.circle")
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .clipShape(Circle())
+                        }
+                        .accessibility(label: Text("Wrong"))
+                        .accessibility(hint: Text("Mark your answer as being incorrect."))
+                        Spacer()
+
+                        Button(action: {
+                            withAnimation {
+                                self.removeCard(at: self.cards.count - 1)
+                            }
+                        }) {
+                            Image(systemName: "checkmark.circle")
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .clipShape(Circle())
+                        }
+                        .accessibility(label: Text("Correct"))
+                        .accessibility(hint: Text("Mark your answer as being correct."))
+                    }
                     .foregroundColor(.white)
                     .font(.largeTitle)
                     .padding()
-                    
                 }
-            }
-        }
-        .onReceive(timer) { time in
-            guard self.isActive else { return }
-            if self.timeRemaining > 0 {
-                self.timeRemaining -= 1
             }
         }
         .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
@@ -90,20 +126,46 @@ struct ContentView: View {
                 self.isActive = true
             }
         }
+        .onReceive(timer) { time in
+            guard self.isActive else { return }
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
+        }
+        .sheet(isPresented: $showingEditScreen, onDismiss: resetCards) {
+            EditCards()
+        }
+        .onAppear(perform: resetCards)
     }
-    
+
     func removeCard(at index: Int) {
+        guard index >= 0 else { return }
+
         cards.remove(at: index)
-        
+
         if cards.isEmpty {
             isActive = false
         }
     }
-    
+
     func resetCards() {
-        cards = [Card](repeating: Card.example, count: 10)
         timeRemaining = 100
         isActive = true
+        loadData()
+    }
+
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                self.cards = decoded
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
@@ -111,11 +173,5 @@ extension View {
     func stacked(at position: Int, in total: Int) -> some View {
         let offset = CGFloat(total - position)
         return self.offset(CGSize(width: 0, height: offset * 10))
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
