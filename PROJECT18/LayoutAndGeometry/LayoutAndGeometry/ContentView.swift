ee9f1b4aba2170cdfb0f37417c854a24d6a7db55
lareenmelo <lareenmmelo@gmail.com>
2021-07-30 23:20:43 -0400

    Add Day 93 / 100

--- PROJECT18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ PROJECT18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Hello, world!")
-            Text("This is a longer line of text")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(width: fullView.size.width)
+                            .background(self.colors[index % 7])
+                            .rotation3DEffect(.degrees(Double(geo.frame(in: .global).minY - fullView.size.height / 2) / 5), axis: (x: 0, y: 1, z: 0))
+
+                    }
+                    .frame(height: 40)
+                }
+            }
         }
-        .background(Color.red)
-        .frame(width: 400, height: 400)
-        .background(Color.blue)
     }
 }
 
--- PROJECT18/README.md
+++ PROJECT18/README.md
@@ -5,12 +5,12 @@ https://www.hackingwithswift.com/100/swiftui/92
 > ...  explore how SwiftUI handles layout
 
 ## Topics
-Alignment, Offset, lastTextBaseline, alignmentGuide, ViewDimensions
+Alignment, Offset, lastTextBaseline, alignmentGuide, ViewDimensions, position(), offset(), GeometryReader, coordinateSpace, ScrollView effects
 
 | Day 92 :white_check_mark: | Day 93 :white_check_mark: | Day 94 :white_check_mark: |
 |:--|:--|:--|
-| learned how layout works in SwiftUI, what alignment guides are and, how to create your own custom alignment guide.  |  |  |
-| 📐 | ![93](Data/D93.png) | ![D94](Data/D94.png) | 
+| learned how layout works in SwiftUI, what alignment guides are and, how to create your own custom alignment guide.  | learned about absolute and relative positions in SwiftUI, explored different ways of using GeometryReader, created custom coordinate spaces using coordinateSpace() and, learned how to create ScrollView effects using GeometryReader |  |
+| 📐 | ![93](Data/D93.mp4) | ![D94](Data/D94.png) | 
 
 
 ## Challenges
